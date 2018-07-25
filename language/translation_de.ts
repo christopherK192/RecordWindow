10885275da2eeb9869ef8995dba20ea86908a5ba
Christopher192 <41643029+christopherK192@users.noreply.github.com>
2018-07-25 18:38:04 +0200

    project files

---
+++ language/translation_de.ts
@@ -0,0 +1,301 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>RecordWindow</name>
+    <message>
+        <location filename="../recordwindow.ui" line="25"/>
+        <source>IVR-Record App</source>
+        <translation>IVR-Record App</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="52"/>
+        <source>Fight Nr.:</source>
+        <translation>Kampf Nr.:</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="161"/>
+        <source>Start Recording</source>
+        <translation>Aufnahme Starten</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="219"/>
+        <source> Start IVR</source>
+        <translation> IVR Starten</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="253"/>
+        <source> Stop IVR</source>
+        <translation> IVR Stoppen</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="282"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Please press &lt;span style=&quot; font-weight:600;&quot;&gt;F4&lt;/span&gt; or &lt;span style=&quot; font-weight:600;&quot;&gt;Stop IVR,&lt;/span&gt; in order to stop the IVR.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Bitte &lt;span style=&quot; font-weight:600;&quot;&gt;F4&lt;/span&gt; oder &lt;span style=&quot; font-weight:600;&quot;&gt;IVR Stoppen&lt;/span&gt; drücken, um das IVR zu beenden.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="315"/>
+        <source>Kamera 1</source>
+        <translation>Kamera 1</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="321"/>
+        <location filename="../recordwindow.ui" line="345"/>
+        <source>Rename File</source>
+        <translation>Datei Umbenennen</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="339"/>
+        <source>Kamera 2</source>
+        <translation>Kamera 2</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="376"/>
+        <source>Programm</source>
+        <translation>Programm</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="384"/>
+        <source>Sprache</source>
+        <translation>Sprache</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="404"/>
+        <source>Deutsch</source>
+        <translation>Deutsch</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="418"/>
+        <location filename="../recordwindow.ui" line="421"/>
+        <location filename="../recordwindow.cpp" line="331"/>
+        <source>Einstellungen</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="424"/>
+        <source>Ctrl+I</source>
+        <translation>Strg+I</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="433"/>
+        <source>Beenden</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="436"/>
+        <source>Ctrl+E</source>
+        <translation>Strg+E</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="448"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="34"/>
+        <source>Start</source>
+        <translation>Aufnahme Starten</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="167"/>
+        <source>Abbrechen</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="229"/>
+        <location filename="../recordwindow.cpp" line="267"/>
+        <source>FEHLER:
+Keine abspielbare Datei gefunden!
+Bitte kontrollieren Sie den Pfad!</source>
+        <translation>FEHLER:
+Keine abspielbare Datei gefunden!
+Bitte kontrollieren Sie den Pfad!</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="385"/>
+        <source>FEHLER:
+Aufnahme konnte nicht gestoppt werden!
+Bitte kontrollieren Sie die Einstellungen!</source>
+        <translation>FEHLER:
+Aufnahme konnte nicht gestoppt werden!
+Bitte kontrollieren Sie die Einstellungen!</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="387"/>
+        <source>FEHLER:
+Aufnahme konnte nicht gestartet werden!
+Bitte kontrollieren Sie die Einstellungen!</source>
+        <translation>FEHLER:
+Aufnahme konnte nicht gestartet werden!
+Bitte kontrollieren Sie die Einstellungen!</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="398"/>
+        <source>Starten</source>
+        <translation>Aufnahme Starten</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="411"/>
+        <source>Stoppen</source>
+        <translation>Aufnahme Stoppen</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="429"/>
+        <source>FEHLER:
+Wiedergabe konnte nicht gestartet werden!
+Bitte kontrollieren Sie die Einstellungen und starten Sie die Wiedergabe manuell!</source>
+        <translation>FEHLER:
+Wiedergabe konnte nicht gestartet werden!
+Bitte kontrollieren Sie die Einstellungen und starten Sie die Wiedergabe manuell!</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="449"/>
+        <source>FEHLER:
+Wiedergabe konnte nicht gestoppt werden!
+Bitte kontrollieren Sie die Einstellungen und stoppen Sie die Wiedergabe manuell!</source>
+        <translation>FEHLER:
+Wiedergabe konnte nicht gestoppt werden!
+Bitte kontrollieren Sie die Einstellungen und stoppen Sie die Wiedergabe manuell!</translation>
+    </message>
+</context>
+<context>
+    <name>SettingDialog</name>
+    <message>
+        <location filename="../settingdialog.ui" line="19"/>
+        <source>Einstellungen</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="32"/>
+        <source>RECORDING</source>
+        <translation>AUFNAHME</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="39"/>
+        <location filename="../settingdialog.ui" line="239"/>
+        <source>-</source>
+        <translation>-</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="66"/>
+        <location filename="../settingdialog.ui" line="231"/>
+        <source>Verbinden</source>
+        <translation>Verbinden</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="83"/>
+        <source>Video Streamer/Recorder:</source>
+        <translation>Video Streamer/Recorder:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="224"/>
+        <source>Video Directory 2:</source>
+        <translation>Video Ordner 2:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="247"/>
+        <source>Video Directory 1:</source>
+        <translation>Video Ordner 1:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="299"/>
+        <source>1 Kamera</source>
+        <translation>1 Kamera</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="325"/>
+        <source>2 Kameras</source>
+        <translation>2 Kameras</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="73"/>
+        <source>Recording Videoformat:</source>
+        <translation>Videoformat:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="99"/>
+        <source>Use vMix System</source>
+        <translation>vMix System</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="128"/>
+        <source>Use FOSCAM System</source>
+        <translation>FOSCAM System</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="170"/>
+        <source>INSTANT VIDEO REPLAY</source>
+        <translation>INSTANT VIDEO REPLAY</translation>
+    </message>
+    <message>
+        <source>Video Directory:</source>
+        <translation type="vanished">Video Ordner:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="264"/>
+        <source>Video Player:</source>
+        <translation>Video Player:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="214"/>
+        <location filename="../settingdialog.ui" line="257"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="369"/>
+        <location filename="../settingdialog.cpp" line="406"/>
+        <source>FEHLER:
+Es konnte keine Instanz des angegebenen Programms gefunden werden!
+Bitte überprüfen Sie, ob das Programm geöffnet ist!</source>
+        <translation>FEHLER:
+Es konnte keine Instanz des angegebenen Programms gefunden werden!
+Bitte überprüfen Sie, ob das Programm geöffnet ist!</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="375"/>
+        <location filename="../settingdialog.cpp" line="412"/>
+        <source>Verbindung mit dem Programm wurde erfolgreich hergestellt.</source>
+        <translation>Verbindung mit dem Programm wurde erfolgreich hergestellt.</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="382"/>
+        <location filename="../settingdialog.cpp" line="419"/>
+        <source>FEHLER:
+Es wurde kein Programm ausgewählt!
+Bitte wählen Sie ein Programm aus!</source>
+        <translation>FEHLER:
+Es wurde kein Programm ausgewählt!
+Bitte wählen Sie ein Programm aus!</translation>
+    </message>
+    <message>
+        <source>FEHLER:
+Es wurde kein Programm ausgewählt!
+Bitte wählen Sie ein Programm aus.</source>
+        <translation type="vanished">FEHLER:
+Es wurde kein Programm ausgewählt!
+Bitte wählen Sie ein Programm aus!</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="432"/>
+        <location filename="../settingdialog.cpp" line="448"/>
+        <source>Ordner Öffnen</source>
+        <translation>Ordner Öffnen</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="437"/>
+        <location filename="../settingdialog.cpp" line="453"/>
+        <source>FEHLER:
+Es wurde kein Ordner ausgewählt!
+Bitte wählen Sie einen Ordner aus!</source>
+        <translation>FEHLER:
+Es wurde kein Ordner ausgewählt!
+Bitte wählen Sie einen Ordner aus!</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="518"/>
+        <source>Einstellungen wurden erfolgreich gespeichert.</source>
+        <translation>Einstellungen wurden erfolgreich gespeichert.</translation>
+    </message>
+</context>
+</TS>
---
+++ language/translation_en.ts
@@ -0,0 +1,293 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>RecordWindow</name>
+    <message>
+        <location filename="../recordwindow.ui" line="25"/>
+        <source>IVR-Record App</source>
+        <translation>IVR-Record App</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="52"/>
+        <source>Fight Nr.:</source>
+        <translation>Fight Nr.:</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="161"/>
+        <source>Start Recording</source>
+        <translation>Start Recording</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="219"/>
+        <source> Start IVR</source>
+        <translation> Start IVR</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="253"/>
+        <source> Stop IVR</source>
+        <translation> Stop IVR</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="282"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Please press &lt;span style=&quot; font-weight:600;&quot;&gt;F4&lt;/span&gt; or &lt;span style=&quot; font-weight:600;&quot;&gt;Stop IVR,&lt;/span&gt; in order to stop the IVR.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Please press &lt;span style=&quot; font-weight:600;&quot;&gt;F4&lt;/span&gt; or &lt;span style=&quot; font-weight:600;&quot;&gt;Stop IVR&lt;/span&gt;, in order to stop the IVR.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="315"/>
+        <source>Kamera 1</source>
+        <translation>Camera 1</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="321"/>
+        <location filename="../recordwindow.ui" line="345"/>
+        <source>Rename File</source>
+        <translation>Rename File</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="339"/>
+        <source>Kamera 2</source>
+        <translation>Camera 2</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="376"/>
+        <source>Programm</source>
+        <translation>Program</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="384"/>
+        <source>Sprache</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="404"/>
+        <source>Deutsch</source>
+        <translation>Deutsch</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="418"/>
+        <location filename="../recordwindow.ui" line="421"/>
+        <location filename="../recordwindow.cpp" line="331"/>
+        <source>Einstellungen</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="424"/>
+        <source>Ctrl+I</source>
+        <translation>Ctrl+I</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="433"/>
+        <source>Beenden</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="436"/>
+        <source>Ctrl+E</source>
+        <translation>Ctrl+E</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.ui" line="448"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="34"/>
+        <source>Start</source>
+        <translation>Start Recording</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="167"/>
+        <source>Abbrechen</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="229"/>
+        <location filename="../recordwindow.cpp" line="267"/>
+        <source>FEHLER:
+Keine abspielbare Datei gefunden!
+Bitte kontrollieren Sie den Pfad!</source>
+        <translation>ERROR:
+No playable file found!
+Please control the directory!</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="385"/>
+        <source>FEHLER:
+Aufnahme konnte nicht gestoppt werden!
+Bitte kontrollieren Sie die Einstellungen!</source>
+        <translation>ERROR:
+Recording could not be stopped!
+Please check the settings!</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="387"/>
+        <source>FEHLER:
+Aufnahme konnte nicht gestartet werden!
+Bitte kontrollieren Sie die Einstellungen!</source>
+        <translation>ERROR:
+Recording could not be started!
+Please check the settings!</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="398"/>
+        <source>Starten</source>
+        <translation>Start Recording</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="411"/>
+        <source>Stoppen</source>
+        <translation>Stop Recording</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="429"/>
+        <source>FEHLER:
+Wiedergabe konnte nicht gestartet werden!
+Bitte kontrollieren Sie die Einstellungen und starten Sie die Wiedergabe manuell!</source>
+        <translation>ERROR:
+Playback could not be started!
+Please check the settings and start the playback manually!</translation>
+    </message>
+    <message>
+        <location filename="../recordwindow.cpp" line="449"/>
+        <source>FEHLER:
+Wiedergabe konnte nicht gestoppt werden!
+Bitte kontrollieren Sie die Einstellungen und stoppen Sie die Wiedergabe manuell!</source>
+        <translation>ERROR:
+Playback could not be stopped!
+Please check the settings and stop the playback manually!</translation>
+    </message>
+</context>
+<context>
+    <name>SettingDialog</name>
+    <message>
+        <location filename="../settingdialog.ui" line="19"/>
+        <source>Einstellungen</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="32"/>
+        <source>RECORDING</source>
+        <translation>RECORDING</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="39"/>
+        <location filename="../settingdialog.ui" line="239"/>
+        <source>-</source>
+        <translation>-</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="66"/>
+        <location filename="../settingdialog.ui" line="231"/>
+        <source>Verbinden</source>
+        <translation>Connect</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="83"/>
+        <source>Video Streamer/Recorder:</source>
+        <translation>Video Streamer/Recorder:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="224"/>
+        <source>Video Directory 2:</source>
+        <translation>Video Directory 2:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="247"/>
+        <source>Video Directory 1:</source>
+        <translation>Video Directory 1:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="299"/>
+        <source>1 Kamera</source>
+        <translation>1 Camera</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="325"/>
+        <source>2 Kameras</source>
+        <translation>2 Cameras</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="73"/>
+        <source>Recording Videoformat:</source>
+        <translation>Video Format:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="99"/>
+        <source>Use vMix System</source>
+        <translation>vMix System</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="128"/>
+        <source>Use FOSCAM System</source>
+        <translation>FOSCAM System</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="170"/>
+        <source>INSTANT VIDEO REPLAY</source>
+        <translation>INSTANT VIDEO REPLAY</translation>
+    </message>
+    <message>
+        <source>Video Directory:</source>
+        <translation type="vanished">Video Directory:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="264"/>
+        <source>Video Player:</source>
+        <translation>Video Player:</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.ui" line="214"/>
+        <location filename="../settingdialog.ui" line="257"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="369"/>
+        <location filename="../settingdialog.cpp" line="406"/>
+        <source>FEHLER:
+Es konnte keine Instanz des angegebenen Programms gefunden werden!
+Bitte überprüfen Sie, ob das Programm geöffnet ist!</source>
+        <translation>ERROR:
+No instance of the specified program could be found!
+Please check if the program is open!</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="375"/>
+        <location filename="../settingdialog.cpp" line="412"/>
+        <source>Verbindung mit dem Programm wurde erfolgreich hergestellt.</source>
+        <translation>Connection to the program was successfully established.</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="382"/>
+        <location filename="../settingdialog.cpp" line="419"/>
+        <source>FEHLER:
+Es wurde kein Programm ausgewählt!
+Bitte wählen Sie ein Programm aus!</source>
+        <translation>ERROR:
+No program was selected!
+Please select a program!</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="432"/>
+        <location filename="../settingdialog.cpp" line="448"/>
+        <source>Ordner Öffnen</source>
+        <translation>Open Folder</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="437"/>
+        <location filename="../settingdialog.cpp" line="453"/>
+        <source>FEHLER:
+Es wurde kein Ordner ausgewählt!
+Bitte wählen Sie einen Ordner aus!</source>
+        <translation>ERROR:
+No folder was selected!
+Please select a folder!</translation>
+    </message>
+    <message>
+        <location filename="../settingdialog.cpp" line="518"/>
+        <source>Einstellungen wurden erfolgreich gespeichert.</source>
+        <translation>Settings have been saved successfully.</translation>
+    </message>
+</context>
+</TS>
